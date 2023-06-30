80646f5c35f2ab347faf7daec70fb7929d8d96b5
JefryTK <jetrix2907@gmail.com>
2023-06-30 16:13:31 -0400

    fix dashboardAdmin

--- app/(presentation)/components/Admin/Dashboard/Summary/SummaryItem/SummaryItem.tsx
+++ app/(presentation)/components/Admin/Dashboard/Summary/SummaryItem/SummaryItem.tsx
@@ -15,7 +15,7 @@ export default function SummaryItem({
   return (
     <div
       className={clsx([
-        "relative zoom-in h-auto",
+        "relative zoom-in h-auto md:h-[180px]",
         "before:content-[''] before:w-[90%] before:shadow-[0px_3px_20px_#0000000b] before:bg-slate-50 bg-slate-50 before:h-full before:mt-3 before:absolute before:rounded-md before:mx-auto before:inset-x-0 before:dark:bg-darkmode-400/70",
       ])}
     >
