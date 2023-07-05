9a128e4dbb8dbd0d7dfa686fd2b518812349dea6
Howard Guzmán <jeremicguzman@gmail.com>
2023-07-05 11:28:49 -0400

    fixed medical record box scroll

--- app/(presentation)/components/core/TopBar/index.tsx
+++ app/(presentation)/components/core/TopBar/index.tsx
@@ -21,7 +21,7 @@ function Main({
   const pathname = usePathname();
 
   return (
-    <div className="h-[67px] z-[51] flex items-center border-b border-slate-200 sticky bg-slate-100 top-0 left-0 w-full">
+    <div className="h-[67px] z-[98] flex items-center border-b border-slate-200 sticky bg-slate-100 top-0 left-0 w-full">
       <Breadcrumb className="hidden mr-auto sm:flex">
         {navigation.map((nav) => (
           <Link key={nav.title} href={nav.pathname}>
