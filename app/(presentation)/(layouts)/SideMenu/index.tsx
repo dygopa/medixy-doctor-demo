f2186367929e2993bc1e5a44037a2fa5fcac0f0e
dygopa <ylansaurio@gmail.com>
2023-08-18 16:25:35 -0400

    Hidded old step-by-step

--- app/(presentation)/(layouts)/SideMenu/index.tsx
+++ app/(presentation)/(layouts)/SideMenu/index.tsx
@@ -93,7 +93,7 @@ function SideMenu({
                 description="Tu sesión se ha expirado, redireccionando..."
               />
             )}
-            {showStepsBySteps && <StepByStep user={data} />}
+            {/* <StepByStep user={data} /> */}
           </div>
           {/* END: Content */}
         </div>
