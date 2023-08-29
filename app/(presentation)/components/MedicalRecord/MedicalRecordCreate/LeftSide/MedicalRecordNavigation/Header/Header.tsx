a525859d25a5f1813a7ca54ccb08a1a2da8e68fa
Howard Guzmán <jeremicguzman@gmail.com>
2023-08-29 14:46:48 -0400

    Medical record locality name

--- app/(presentation)/components/MedicalRecord/MedicalRecordCreate/LeftSide/MedicalRecordNavigation/Header/Header.tsx
+++ app/(presentation)/components/MedicalRecord/MedicalRecordCreate/LeftSide/MedicalRecordNavigation/Header/Header.tsx
@@ -1,15 +1,30 @@
+import { useContext } from "react";
+import {
+  IMedicalRecordCreateContext,
+  MedicalRecordCreateContext,
+} from "../../../context/MedicalRecordCreateContext";
+
 export default function Header() {
+  const { state } = useContext<IMedicalRecordCreateContext>(
+    MedicalRecordCreateContext
+  );
+  const { data: appointment } = state.appointment;
+
   return (
     <div className="w-full">
       <div className="lg:flex items-center justify-between">
         <div>
-          <div className="flex items-center">
+          <div className="mb-3">
             <div className="mr-4">
               <p className="text-slate-500 text-md">Consultorio</p>
             </div>
 
             <div>
-              <p className="text-slate-900 text-lg">Coyoacán</p>
+              <p className="text-slate-900 font-bold text-md">
+                {appointment?.data && appointment.data.service.location
+                  ? appointment.data.service.location.name
+                  : ""}
+              </p>
             </div>
           </div>
 
