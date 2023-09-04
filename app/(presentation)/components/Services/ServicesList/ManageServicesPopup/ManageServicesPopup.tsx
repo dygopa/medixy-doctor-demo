22acdd66c601ce6443d7df92d07f2cdff35bc6a3
Howard Guzmán <jeremicguzman@gmail.com>
2023-09-04 16:14:03 -0400

    Step services

--- app/(presentation)/components/Services/ServicesList/ManageServicesPopup/ManageServicesPopup.tsx
+++ app/(presentation)/components/Services/ServicesList/ManageServicesPopup/ManageServicesPopup.tsx
@@ -1,7 +1,48 @@
+import {
+  AuthContext,
+  IAuthContext,
+} from "(presentation)/(layouts)/AppLayout/context/AuthContext";
+import Button from "(presentation)/components/core/BaseComponents/Button";
+import {
+  IStepByStepContext,
+  StepByStepContext,
+} from "(presentation)/components/core/StepByStepPopup/context/StepByStepContext";
+import { useContext, useEffect, useState } from "react";
+
 export default function ManageServicesPopup() {
+  const { state: authState } = useContext<IAuthContext>(AuthContext);
+  const { data: user } = authState.getUserAuthenticated;
+
+  const { state, dispatch, actions } =
+    useContext<IStepByStepContext>(StepByStepContext);
+  const { createUserSteps } = actions;
+  const { data: steps, loading } = state.getStepsMessages;
+  const { successful } = state.createUserSteps;
+
+  const [stepIsCompleted, setStepIsCompleted] = useState(false);
+
+  const onStepCompleted = () =>
+    createUserSteps(user.accountId, "SERVICE_UPDATED")(dispatch);
+
+  const getStepIsCompleted = () => {
+    const serviceEvent = steps.findIndex(
+      (event: any) => event.evento === "SERVICE_UPDATED"
+    );
+
+    if (serviceEvent >= 0) setStepIsCompleted(true);
+  };
+
+  useEffect(() => {
+    if (steps && steps.length > 0) getStepIsCompleted();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [steps]);
+
+  if (loading || stepIsCompleted || !user?.accountId || successful)
+    return <div />;
+
   return (
     <div className="fixed bottom-4 right-4">
-      <div className="w-[450px] h-[400px] bg-white rounded-md p-4">
+      <div className="w-[450px] h-[200px] bg-white rounded-md shadow-md p-4">
         <div>
           <h2 className="text-slate-900 font-bold text-lg">
             Gestión de servicios
@@ -13,6 +54,16 @@ export default function ManageServicesPopup() {
             Acá podrás gestionar todos tus servicios creando, editando y
             eliminando los que sean necesarios para tus consultorios digitales.
           </p>
+        </div>
+
+        <div className="mt-8">
+          <Button
+            variant="primary"
+            className="w-full"
+            onClick={() => onStepCompleted()}
+          >
+            Entendido
+          </Button>
         </div>
       </div>
     </div>
--- app/(presentation)/components/Services/ServicesList/Table/Table.tsx
+++ app/(presentation)/components/Services/ServicesList/Table/Table.tsx
@@ -4,7 +4,7 @@ import { OrdersRoutesEnum } from "(presentation)/(routes)/ordersRoutes";
 import Paginate from "(presentation)/components/core/Paginate/Paginate";
 import Link from "next/link";
 import Lucide from "(presentation)/components/core/BaseComponents/Lucide";
-import { useContext, useMemo } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 import {
   IServicesContext,
   ServicesContext,
@@ -13,16 +13,85 @@ import { IService } from "domain/core/entities/serviceEntity";
 import Table from "(presentation)/components/core/BaseComponents/Table";
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
+import {
+  IStepByStepContext,
+  StepByStepContext,
+} from "(presentation)/components/core/StepByStepPopup/context/StepByStepContext";
 
 export default function TableServices({ user }: { user: IUser }) {
+  const {
+    state: stepByStepState,
+    dispatch: stepByStepDispatch,
+    actions: stepByStepActions,
+  } = useContext<IStepByStepContext>(StepByStepContext);
+  const { createUserSteps } = stepByStepActions;
+  const { data: steps, loading: getStepsLoading } =
+    stepByStepState.getStepsMessages;
+  const { successful: createStepSucessful } = stepByStepState.createUserSteps;
+
   const { state, actions, dispatch } =
     useContext<IServicesContext>(ServicesContext);
   const { data, loading, successful, error } = state.getUserServices;
   const { getUserServices } = actions;
 
+  const [stepIsCompleted, setStepIsCompleted] = useState(false);
+
+  const onStepCompleted = () =>
+    createUserSteps(user.accountId, "SERVICE_UPDATED")(dispatch);
+
+  const getStepIsCompleted = () => {
+    const serviceEvent = steps.findIndex(
+      (event: any) => event.evento === "SERVICE_UPDATED"
+    );
+
+    if (serviceEvent >= 0) setStepIsCompleted(true);
+  };
+
+  useEffect(() => {
+    if (steps && steps.length > 0) getStepIsCompleted();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [steps]);
+
+  useEffect(() => {
+    if (createStepSucessful) {
+      setStepIsCompleted(true);
+    }
+  }, [createStepSucessful]);
+
   const router = useRouter();
 
+  useMemo(() => {
+    if (user.userId) getUserServices(user.userId)(dispatch);
+  }, [user.userId]);
+
+  if (getStepsLoading || !user?.accountId) return <div />;
+
   const TableData = ({ data }: { data: IService }) => {
+    if (!stepIsCompleted) {
+      return (
+        <button
+          onClick={() => onStepCompleted()}
+          className="cursor-pointer w-full grid grid-cols-4 items-center justify-items-start bg-white px-3 py-4 border rounded-lg"
+        >
+          <p className="border-b-0 whitespace-nowrap text-sm font-medium text-slate-900">
+            {data.name}
+          </p>
+          <p className="border-b-0 whitespace-nowrap text-sm font-medium text-slate-900">
+            {data.service_category.name}
+          </p>
+          <p className="border-b-0 whitespace-nowrap text-sm font-medium text-slate-900">
+            <div className="w-full flex justify-start items-center gap-2">
+              <span className="rounded-full w-[12px] h-[12px] bg-success"></span>
+              <p className="text-sm font-medium text-slate-900">Activo</p>
+            </div>
+          </p>
+          <p className="border-b-0 whitespace-nowrap text-sm font-medium text-slate-900">
+            ${data.base_price}
+          </p>
+        </button>
+      );
+    }
+
     return (
       <Link
         href={`/services/${data.id}`}
@@ -50,6 +119,41 @@ export default function TableServices({ user }: { user: IUser }) {
   console.log(data);
 
   const CardData = ({ data }: { data: IService }) => {
+    if (!stepIsCompleted) {
+      return (
+        <button
+          onClick={() => onStepCompleted()}
+          className="bg-white border rounded-lg p-4 flex flex-col justify-between items-start gap-4 cursor-pointer"
+        >
+          <div className="w-full flex justify-start items-center gap-4">
+            <div className="relative flex flex-col justify-center items-start">
+              <p className="font-semibold text-lg text-gray-950">{data.name}</p>
+              <p className="font-light text-sm text-slate-500">
+                Categoria: {data.service_category.name}
+              </p>
+            </div>
+          </div>
+          <div className="w-full grid grid-cols-3 gap-1">
+            <div className="flex flex-col justify-start items-start gap-2 text-left">
+              <p className="font-light text-gray-500 text-sm">Precio base</p>
+              <p className="font-normal text-gray-950 text-base">
+                ${data.base_price}
+              </p>
+            </div>
+            <div className="flex flex-col justify-start items-start gap-2 text-left">
+              <p className="border-b-0 whitespace-nowrap text-sm font-medium text-slate-900">
+                <p className="font-light text-gray-500 text-sm mb-3">Estatus</p>
+                <div className="w-full flex justify-start items-center gap-2">
+                  <span className="rounded-full w-[12px] h-[12px] bg-success"></span>
+                  <p className="text-sm font-medium text-slate-900">Activo</p>
+                </div>
+              </p>
+            </div>
+          </div>
+        </button>
+      );
+    }
+
     return (
       <>
         <Link
@@ -116,7 +220,11 @@ export default function TableServices({ user }: { user: IUser }) {
               [...(data as Array<IService>)].map((service, i) => (
                 <Table.Tr
                   key={service.id}
-                  onClick={() => router.push(`/services/${service.id}`)}
+                  onClick={
+                    stepIsCompleted
+                      ? () => router.push(`/services/${service.id}`)
+                      : () => onStepCompleted()
+                  }
                   className="bg-white cursor-pointer"
                 >
                   <Table.Td>
@@ -134,13 +242,17 @@ export default function TableServices({ user }: { user: IUser }) {
                   <Table.Td>
                     <p className="border-b-0 whitespace-nowrap text-sm font-medium text-slate-900">
                       <div className="w-full flex justify-start items-center gap-2">
-                        <span className={twMerge([
-                          "rounded-full w-[12px] h-[12px]",
-                          service.status === 1 ? "bg-success" : "bg-warning"
-                        ])}></span>
-                        <p className={twMerge([
-                          "text-sm font-medium text-slate-900"
-                        ])}>
+                        <span
+                          className={twMerge([
+                            "rounded-full w-[12px] h-[12px]",
+                            service.status === 1 ? "bg-success" : "bg-warning",
+                          ])}
+                        ></span>
+                        <p
+                          className={twMerge([
+                            "text-sm font-medium text-slate-900",
+                          ])}
+                        >
                           {service.status === 1 ? "Activo" : "Borrador"}
                         </p>
                       </div>
@@ -184,10 +296,6 @@ export default function TableServices({ user }: { user: IUser }) {
       </div>
     );
   };
-
-  useMemo(() => {
-    if (user.userId) getUserServices(user.userId)(dispatch);
-  }, [user.userId]);
 
   if (loading) {
     return (
