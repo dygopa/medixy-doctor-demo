c16e270d7e88130db2e9c4a57633c56f624be394
dygopa <ylansaurio@gmail.com>
2023-09-05 14:41:15 -0400

    Fixed asyncronus creation of appointment

--- app/domain/useCases/schedule/scheduleUseCase.ts
+++ app/domain/useCases/schedule/scheduleUseCase.ts
@@ -159,12 +159,21 @@ export default class ScheduleUseCase {
         
         obj["pacienteId"] = responsePatient.subjectId
 
+        const response = await this._repository.createAppointment({
+          ...obj,
+          pacienteId: responsePatient.subjectId
+        }, now);
+        if (response instanceof ScheduleFailure) throw response;
+
+        return response;
+      }else{
+
+        const response = await this._repository.createAppointment(obj, now);
+        if (response instanceof ScheduleFailure) throw response;
+
+        return response;
       }
       
-      const response = await this._repository.createAppointment(obj, now);
-      if (response instanceof ScheduleFailure) throw response;
-      
-      return response;
     } catch (error) {
       throw error;
     }
