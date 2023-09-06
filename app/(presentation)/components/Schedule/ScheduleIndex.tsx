b925f15e0dc05db2b7e7fdf33472ca97234efa30
Howard Guzmán <jeremicguzman@gmail.com>
2023-09-06 07:54:09 -0400

    Schedule user error fix

--- app/(presentation)/components/Schedule/ScheduleIndex.tsx
+++ app/(presentation)/components/Schedule/ScheduleIndex.tsx
@@ -5,21 +5,34 @@ import Filters from "./Filters/Filters";
 import Popup from "../core/Popup/Popup";
 import Navigator from "./Navigator/Navigator";
 import CalendarIndex from "./Calendar/CalendarIndex";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import {
+  AuthContext,
+  IAuthContext,
+} from "(presentation)/(layouts)/AppLayout/context/AuthContext";
 
 export default function ScheduleIndex() {
+  const { state } = useContext<IAuthContext>(AuthContext);
+  const { data: user } = state.getUserAuthenticated;
+
   const [selectedLocality, setSelectedLocality] = useState({
     id: 0,
     title: "",
     description: "",
   });
+
+  if (!user?.userId) return <div />;
+
   return (
     <div className="container pt-8">
       <ScheduleProvider>
         <Navigator selectedLocality={selectedLocality} />
-        <Popup/>
-        <Filters selectedLocality={selectedLocality} setSelectedLocality={setSelectedLocality}/>
-        <CalendarIndex/>
+        <Popup />
+        <Filters
+          selectedLocality={selectedLocality}
+          setSelectedLocality={setSelectedLocality}
+        />
+        <CalendarIndex />
       </ScheduleProvider>
     </div>
   );
