85377fb4b3696a652ec8a8d98a5dce6a3d3a7556
dygopa <ylansaurio@gmail.com>
2023-11-20 12:39:42 -0400

    Lists commit

--- app/(presentation)/components/Admin/Metrics/Body/Services/List/List.tsx
+++ app/(presentation)/components/Admin/Metrics/Body/Services/List/List.tsx
@@ -8,17 +8,29 @@ const List = () => {
         {
             title: "Imagenología",
             total: "24%",
-            byService: 100000
+            byService: 100000,
+            quantityOrder: 200,
+            percentage: "40%",
+            averagePriceService: 10000,
+            amountIssuedDoctors: 1000
         },
         {
             title: "Imagenología",
             total: "24%",
-            byService: 100000
+            byService: 100000,
+            quantityOrder: 200,
+            percentage: "40%",
+            averagePriceService: 10000,
+            amountIssuedDoctors: 1000
         },
         {
             title: "Imagenología",
             total: "24%",
-            byService: 100000
+            byService: 100000,
+            quantityOrder: 200,
+            percentage: "40%",
+            averagePriceService: 10000,
+            amountIssuedDoctors: 1000
         },
     ]
 
@@ -35,6 +47,18 @@ const List = () => {
                     <Table.Th className="border-b-0 whitespace-nowrap text-base">
                         Total acumulado por servicio
                     </Table.Th>
+                    <Table.Th className="border-b-0 whitespace-nowrap text-base">
+                        Cantidad órdenes contratados en la clínica
+                    </Table.Th>
+                    <Table.Th className="border-b-0 whitespace-nowrap text-base">
+                        % realizado en la clínica
+                    </Table.Th>
+                    <Table.Th className="border-b-0 whitespace-nowrap text-base">
+                        Precio promedio servicio
+                    </Table.Th>
+                    <Table.Th className="border-b-0 whitespace-nowrap text-base">
+                        Cantidad emitidas por médicos
+                    </Table.Th>
                 </Table.Tr>
             </Table.Thead>
 
@@ -46,23 +70,43 @@ const List = () => {
                         onClick={()=>{}}
                         className="bg-white cursor-pointer"
                     >
+                        
                         <Table.Td>
                             <p className="border-b-0 whitespace-nowrap text-sm font-medium text-slate-900">
                                 {elem["title"]}
                             </p>
                         </Table.Td>
-
                         <Table.Td>
                             <p className="border-b-0 whitespace-nowrap text-sm font-medium text-slate-900">
                                 {elem["total"]}
                             </p>
                         </Table.Td>
-
                         <Table.Td>
                             <p className="border-b-0 whitespace-nowrap text-sm font-medium text-slate-900">
                                 {elem["byService"]}
                             </p>
                         </Table.Td>
+                        <Table.Td>
+                            <p className="border-b-0 whitespace-nowrap text-sm font-medium text-slate-900">
+                                {elem["quantityOrder"]}
+                            </p>
+                        </Table.Td>
+                        <Table.Td>
+                            <p className="border-b-0 whitespace-nowrap text-sm font-medium text-slate-900">
+                                {elem["percentage"]}
+                            </p>
+                        </Table.Td>
+                        <Table.Td>
+                            <p className="border-b-0 whitespace-nowrap text-sm font-medium text-slate-900">
+                                {elem["averagePriceService"]}
+                            </p>
+                        </Table.Td>
+                        <Table.Td>
+                            <p className="border-b-0 whitespace-nowrap text-sm font-medium text-slate-900">
+                                {elem["amountIssuedDoctors"]}
+                            </p>
+                        </Table.Td>
+
                     </Table.Tr>
                 ))
             }
